6cdd077b8b9005dceb7646de0ebd80750da0b718
Vitaliy <kv111nt@gmail.com>
2017-02-24 10:16:17 +0200

    Return_to_last

--- StudService1/Migrations/201702170713275_ChangeTypePhoneNumberField.cs
+++ StudService1/Migrations/201702170713275_ChangeTypePhoneNumberField.cs
@@ -2,14 +2,18 @@ namespace StudService1.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class ChangeTypePhoneNumberField : DbMigration
     {
         public override void Up()
         {
+            Sql(@"ALTER TABLE dbo.Phones
+                DROP CONSTRAINT IF EXISTS DF__Phones__PhoneNum__3C69FB99
+                GO");
+            //DropIndex("dbo.Phones", "DF__Phones__PhoneNum__3C69FB99");
             AlterColumn("dbo.Phones", "PhoneNumber", c => c.String(nullable: false));
         }
-        
+
         public override void Down()
         {
             AlterColumn("dbo.Phones", "PhoneNumber", c => c.Int(nullable: false));
